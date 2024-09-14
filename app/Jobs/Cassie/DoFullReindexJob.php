daa4c40c6e9cea264e66ecac09a79616f68bb3c6
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-09-14 18:48:02 +0700

    Pass User to the DoFullReindexJob.php

--- app/Jobs/Cassie/DoFullReindexJob.php
+++ app/Jobs/Cassie/DoFullReindexJob.php
@@ -2,18 +2,18 @@
 
 namespace App\Jobs\Cassie;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Shopify\Auth\Session as ShopifySession;
 
 class DoFullReindexJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(public ShopifySession $session)
+    public function __construct(public User $user)
     {
     }
 
