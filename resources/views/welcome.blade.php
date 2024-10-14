297dd9e2919cd12790aae9189692c283aad6b11f
Oleg Shabashov <oleg.shabashov@gmail.com>
2024-10-14 15:04:36 +0700

    Add reindex button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <!-- You are: (shop domain name) -->
     <p>You are: {{ $shopDomain ?? Auth::user()->name }}</p>
     <a href="{{ url('/admin') }}">Go to Admin</a>
+    <a href="{{ url('/do-full-reindex') }}">Reindex everything</a>
 @endsection
 
 @section('scripts')
